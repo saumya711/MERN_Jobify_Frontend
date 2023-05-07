bccd4e3958d85be4022fd2eb2c93d662f3ae4ef8
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-05-08 00:02:02 +0530

    CHG | Register User- Setup

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,12 +1,21 @@
 import React, { useContext, useReducer} from "react";
 import reducer from "./reducer";
-import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
+import { 
+    DISPLAY_ALERT, 
+    CLEAR_ALERT,
+    REGISTER_USER_BEGIN,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_ERROR
+ } from "./actions"
 
 const initialState = {
     isLoading: false,
     showAlert: false,
     alertText: '',
     alertType: '',
+    user: null,
+    taken: null,
+    userLOcation: '',
 }
 
 const AppContext =  React.createContext()
@@ -24,8 +33,12 @@ const AppProvider = ({children}) => {
             dispatch({ type: CLEAR_ALERT})
         }, 3000);
     }
+
+    const registerUser = async (currentUser) => {
+        console.log(currentUser)
+    }
     return (
-        <AppContext.Provider value={{ ...state, displayAlert}}>{children}</AppContext.Provider>
+        <AppContext.Provider value={{ ...state, displayAlert, registerUser}}>{children}</AppContext.Provider>
     )
 }
 
