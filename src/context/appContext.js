396f3dccdf844571b2832fd670fd603a353913b4
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-05-12 22:06:45 +0530

    CHG | Register user function

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,5 +1,6 @@
 import React, { useContext, useReducer} from "react";
 import reducer from "./reducer";
+import axios from 'axios';
 import { 
     DISPLAY_ALERT, 
     CLEAR_ALERT,
@@ -16,6 +17,7 @@ const initialState = {
     user: null,
     taken: null,
     userLOcation: '',
+    jobLocation: '',
 }
 
 const AppContext =  React.createContext()
@@ -35,7 +37,24 @@ const AppProvider = ({children}) => {
     }
 
     const registerUser = async (currentUser) => {
-        console.log(currentUser)
+        dispatch({ type: REGISTER_USER_BEGIN})
+        try {
+            const response = await axios.post('/api/v1/auth/register', currentUser)
+            console.log(response)
+            const { user, token, location } = response.data
+            dispatch({ 
+                type: REGISTER_USER_SUCCESS,
+                payload: { user, token, location},
+            })
+            // local srorage later
+        } catch (error) {
+            console.log(error.response)
+            dispatch({
+                type: REGISTER_USER_ERROR,
+                payload: { msg: error.response.data.msg },
+            }) 
+        }
+        clearAlert()
     }
     return (
         <AppContext.Provider value={{ ...state, displayAlert, registerUser}}>{children}</AppContext.Provider>
