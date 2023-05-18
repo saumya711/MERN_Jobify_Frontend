454b601a5ed95908676c69f735bd627c04f2a8b3
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-05-18 22:07:53 +0530

    CHG | Protected Route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Landing from "./Landing";
 import Register from "./Register";
 import Error from "./Error";
+import ProtectedRoute from "./ProtectedRoute";
 
 export {
-    Landing, Register, Error
+    Landing, Register, Error, ProtectedRoute
 }
