847e9fd9a3421ba6863341333635f13a1d83f3e6
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-05-22 08:07:24 +0530

    CHG | Page Structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 import FormRow from "./FormRow";
 import Logo from "./Logo";
 import Alert from "./Alert";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar"
+import BigSidebar from "./BigSidebar"
 
-export { Logo, FormRow, Alert }
+
+export { Logo, FormRow, Alert, Navbar, SmallSidebar, BigSidebar }
