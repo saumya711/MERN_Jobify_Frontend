056fccb96b238c74e1c2e0aca4087cedb2223021
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-06-09 22:21:16 +0530

    CHG | Small Sidebar-Setup

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import Wrapper from '../assets/wrappers/SmallSidebar'
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes} from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import Links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo'
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h1>Small Sidebar</h1>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button 
+            type="button"
+            className='close-btn'
+            onClick={() => console.log('toggle sidebar')}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>nav links</div>
+        </div>
+
+      </div>
     </Wrapper>
   )
 }
