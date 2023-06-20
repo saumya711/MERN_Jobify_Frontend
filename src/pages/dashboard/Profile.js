514d68a7c710a15eff551f3d00fb066397f6ddd6
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-06-20 09:06:28 +0530

    CHG | Profile Page2

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { FormRow, Alert } from '../../components';
+import { useState, useEffect } from 'react';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
 
 const Profile = () => {
+  const {user, showAlert, displayAlert, isLoading, updateUser} = useAppContext()
+  const [name, setName] = useState(user?.name)
+  const [emai, setEmail] = useState(user?.emai)
+  const [lastName, setLastName] = useState(user?.lastName)
+  const [location, setLocation] = useState(user?.location)
   return (
     <h1>
       Profile Page
