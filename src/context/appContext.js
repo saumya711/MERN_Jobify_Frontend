d28180922e74725b288755f09f4b706f6ecf9bf3
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-06-22 21:59:01 +0530

    CHG | Axios Custom instance

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -39,7 +39,12 @@ const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     //axios
-    axios.defaults.headers.common['Authorization'] = `Bearer ${state.token}`
+    const authFetch = axios.create({
+        baseURL: 'api/v1',
+        headers: {
+            Authorization: `Bearer ${state.token}`
+        }
+    })
 
     const displayAlert = () => {
         dispatch({ type: DISPLAY_ALERT });
@@ -138,7 +143,7 @@ const AppProvider = ({children}) => {
 
     const updateUser = async (currentUser) => {
         try {
-            const { data } = await axios.patch('/api/v1/auth//update-user', currentUser)
+            const { data } = await authFetch.patch('/auth//update-user', currentUser)
             console.log(data)
         } catch (error) {
             console.log(error.response)
