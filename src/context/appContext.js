741d77d77347ec461a07366523950b0021f21538
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2023-06-26 23:19:25 +0530

    CHG | Axios Interceptor

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -46,6 +46,31 @@ const AppProvider = ({children}) => {
         }
     })
 
+    // request
+    authFetch.interceptors.response.use(
+        (response) =>{
+            return response
+        },
+        (error) =>{
+            console.log(error.response)
+            if(error.response.status === 401) {
+                console.log('AUTH ERROR')
+            }
+            return Promise.reject(error)
+        }
+    )
+
+    // request
+    authFetch.interceptors.request.use(
+        (config) =>{
+            config.headers.common['Authorization'] = `Bearer ${state.token}`
+            return config
+        },
+        (error) =>{
+            return Promise.reject(error)
+        }
+    )
+
     const displayAlert = () => {
         dispatch({ type: DISPLAY_ALERT });
         clearAlert()
